d9129643565cb5fd8f28409228f103ad03dde287
fredosantana <santana.wil@gmail.com>
2018-02-07 15:32:13 -0500

    Buttons to create new bars and restaurants added to show pages

--- seeds.js
+++ seeds.js
@@ -3,50 +3,18 @@ const mongoose = require('mongoose'),
       Restaurant = require('./models/restaurants'),
       Comment  = require('./models/comments');
 
-let bars = [
-  {
-    name: "Bourbon Pub and Parade",
-    image: "http://www.neworleansonline.com/images/slideshows/listings/1106/04.jpg",
-    description: "One of my favorite bars"
-  },
-  {
-    name: "Old Absinthe House",
-    image: "http://www.hauntedhouses.com/photos/new-orleans-absinthe-68.jpg",
-    description: "I gotta try this place soon"
-  },
-  {
-    name: "Oz",
-    image: "https://www.outtraveler.com/sites/outtraveler.com/files/2016/01/28/oz.jpg",
-    description: "Another one I go to when I'm in NOLA"
-  }
-];
-let restaurants = [
-  {
-    name: "Oceana",
-    image: "https://1.bp.blogspot.com/-F9vegPn55og/TsktWL0_WLI/AAAAAAAAArY/6n3Q9zbAjaQ/w1200-h630-p-k-no-nu/oceana.jpg",
-    description: "First stop for any and all NOLA trips"
-  },
-  {
-    name: "Red Fish Grill",
-    image: "http://www.bunkycooks.com/wp-content/uploads/2011/03/Red-Fish-Grill-sign.jpg",
-    description: "Fine NOLA dining in a casual setting"
-  },
-  {
-    name: "Port of Call",
-    image: "https://media-cdn.tripadvisor.com/media/photo-s/0f/18/7b/62/port-of-call.jpg",
-    description: "Excellent burger joint in the heart of the Treme neighborhood"
-  }
-];
+let bars = [];
+let restaurants = [];
 
 function seedDB() {
   // Remove all bars
-  Bar.remove({}, (err) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log("Removed Bar!");
-    }
-  });
+  // Bar.remove({}, (err) => {
+  //   if (err) {
+  //     console.log(err);
+  //   } else {
+  //     console.log("Removed Bar!");
+  //   }
+  // });
   // Add a new bar
   bars.forEach((seed) => {
     Bar.create(seed, (err, bars) => {
@@ -72,13 +40,13 @@ function seedDB() {
     });
   });
 
-  Restaurant.remove({}, (err) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log("Removed Restaurant!");
-    }
-  });
+  // Restaurant.remove({}, (err) => {
+  //   if (err) {
+  //     console.log(err);
+  //   } else {
+  //     console.log("Removed Restaurant!");
+  //   }
+  // });
 
   // Add a few comments
   // Add a new restaurant
